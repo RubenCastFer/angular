111068ac3214f6d934f9ca5604b26d244d9ac0c7
RubenCastFer <ruben.castellano.fernandez.al@iespoligonosur.org>
2022-02-12 15:33:56 +0100

    alumnos funciona

--- primerProyecto-dwc/src/app/alumnado-daw/alumnado-daw.component.ts
+++ primerProyecto-dwc/src/app/alumnado-daw/alumnado-daw.component.ts
@@ -14,10 +14,46 @@ export class AlumnadoDAWComponent implements OnInit {
 
     this.alumnos=[new AlumnadoDAW("Ruben","Castellano","48963910s",new Date("1996-06-01"),"Dos Hermanas", 679051051,2,["dwc","dws","dwi"]),
                   new AlumnadoDAW("Manuel","Fernandez","48963911s",new Date("1997-07-01"),"Sevilla", 679051052,2,["dwc","dwi"]),
+                  new AlumnadoDAW("David","Peñasco","52963910s",new Date("2000-03-05"),"Triana", 679051554,1,["Programacion"]),
                   new AlumnadoDAW("David","Peñasco","52963910s",new Date("2000-03-05"),"Triana", 679051554,1,["Programacion"])]
   }
 
-  ngOnInit(): void {
+  public mostrarPrimero(): AlumnadoDAW[]{
+    var alumnosPrimero:Array<AlumnadoDAW>=[];
+    var z=0;
+    for (let i = 0; i < this.alumnos.length; i++) {
+      if (this.alumnos[i].curso==1) {
+        alumnosPrimero[z]=this.alumnos[i];
+        z++
+      }
+    }
+    return alumnosPrimero;
   }
 
+  public mostrarSegundo(): AlumnadoDAW[]{
+    var alumnosSegundo:Array<AlumnadoDAW>=[];
+    var z=0;
+    for (let i = 0; i < this.alumnos.length; i++) {
+      if (this.alumnos[i].curso==2) {
+        alumnosSegundo[z]=this.alumnos[i];
+        z++;
+      }
+    }
+    return alumnosSegundo;
+  }
+  
+  /* Component */
+  public isShownPrimero: boolean | undefined;
+  public isShownSegundo: boolean | undefined;
+
+ngOnInit(){
+  this.isShownPrimero = false; //hidden every time subscribe detects change
+}
+
+toggleShowPrimero() {
+  this.isShownPrimero = ! this.isShownPrimero;
+}
+toggleShowSegundo() {
+  this.isShownSegundo = ! this.isShownSegundo;
+}
 }
