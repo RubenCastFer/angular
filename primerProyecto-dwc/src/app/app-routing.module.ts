cda97860916669f2ddcdc9c75853125f18654e05
RubenCastFer <ruben.castellano.fernandez.al@iespoligonosur.org>
2022-02-15 23:17:49 +0100

    rutas

--- primerProyecto-dwc/src/app/app-routing.module.ts
+++ primerProyecto-dwc/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmpleadoComponent } from './empleado/empleado.component';
+import { CocheComponent } from './coche/coche.component';
+import { HomeComponent } from './home/home.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { AlumnadoDAWComponent } from './alumnado-daw/alumnado-daw.component';
 
 const routes: Routes = [
   {path: 'empleado', component: EmpleadoComponent},
+  {path: 'coche', component: CocheComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'contacto', component: ContactoComponent},
+  {path: 'alumnadoDaw', component: AlumnadoDAWComponent},
 
   //Estas dos rutas deben ir al final
   {path: '', component: EmpleadoComponent},
