4d60cc571424ddc486d43e09b58ad3b78a8c93af
RubenCastFer <ruben.castellano.fernandez.al@iespoligonosur.org>
2022-02-28 04:51:29 +0100

    rutas

--- proyectoAngularDWC/src/app/app-routing.module.ts
+++ proyectoAngularDWC/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChuckNorrisComponent } from './chuck-norris/chuck-norris.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: 'chuck-norris',component: ChuckNorrisComponent},
+  {path: 'home',component: HomeComponent},
+  {path: 'formulario',component: FormularioComponent},
 
-  {path: '', component: ChuckNorrisComponent},
-  {path: '**',component: ChuckNorrisComponent}
+  {path: '', component: HomeComponent},
+  {path: '**',component: HomeComponent}
 ];
 
 @NgModule({
--- proyectoAngularDWC/src/app/app.module.ts
+++ proyectoAngularDWC/src/app/app.module.ts
@@ -7,11 +7,15 @@ import { ChuckNorrisComponent } from './chuck-norris/chuck-norris.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChuckNorrisComponent
+    ChuckNorrisComponent,
+    HomeComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
